ac211e4db9aff2db9638489aca5f04d30da96c1d
k0nghaa <k0nghaalee@gmail.com>
2026-04-17 02:44:07 +0900

    refactor(MP-697): 소셜 로그인 응답 타입 범용화 및 플랫폼 접두사 제거

--- lib/api/client/oauth.ts
+++ lib/api/client/oauth.ts
@@ -5,7 +5,7 @@ import { AUTH_ENDPOINTS } from '@/lib/constants/endpoints';
  * 소셜 인증 API
  */
 
-type KakaoLoginResponse =
+type SocialLoginResponse =
   | { type: 'LOGIN'; accessToken: string }
   | { type: 'NEED_SIGNUP'; email: string; nickname: string }
   | { type: 'NEED_LINK'; email: string; nickname: string };
@@ -22,8 +22,8 @@ export const OauthApi = {
   /**
    * 카카오 로그인
    */
-  async kakaoLogin(code: string): Promise<ApiResponse<KakaoLoginResponse>> {
-    const response = await clientApiInstance.post<KakaoLoginResponse>(
+  async kakaoLogin(code: string): Promise<ApiResponse<SocialLoginResponse>> {
+    const response = await clientApiInstance.post<SocialLoginResponse>(
       AUTH_ENDPOINTS.KAKAO_LOGIN,
       { code },
       { skipAuth: true }
