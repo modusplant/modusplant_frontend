edf48cdd0b7fc3eef91f76970d744b4ce7df359d
k0nghaa <k0nghaalee@gmail.com>
2026-04-18 22:49:42 +0900

    feat(MP-703): 소셜-소셜 로그인 시도 시 에러 메시지 모달 추가

--- lib/constants/oauth.ts
+++ lib/constants/oauth.ts
@@ -14,3 +14,8 @@ const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 const GOOGLE_REDIRECT_URL = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL;
 export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URL}&response_type=code&scope=email profile`;
 // TODO: state 매개변수 추가
+
+export const SOCIAL_CONFLICT_MESSAGES: Record<string, string> = {
+  already_registered_with_google: '당신은 구글로 가입했습니다.',
+  already_registered_with_kakao: '당신은 카카오로 가입했습니다.',
+};
