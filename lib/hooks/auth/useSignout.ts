2f062973bb8d53d5e3670753d12f24e3eaa6f522
aggie97 <4ggie1997@gmail.com>
2026-04-24 13:39:53 +0900

    feat(MP-708): 회원 탈퇴 UI 추가

--- lib/hooks/auth/useSignout.ts
+++ lib/hooks/auth/useSignout.ts
@@ -6,21 +6,32 @@ import { useRouter } from 'next/navigation';
 import { useAuthStore } from '@/lib/store/authStore';
 import { authApi } from '@/lib/api/client/auth';
 import { SignoutRequestBody } from '@/lib/types/auth';
+import { SignoutFormValues } from '@/components/mypage/account/SignoutForm';
+import { deleteAllCookies } from '@/lib/utils/cookies/client';
 
 export const useSignout = () => {
   const queryClient = useQueryClient();
+
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<ApiError | null>(null);
 
-  const requestSignout = async (requestBody: SignoutRequestBody) => {
+  const handleSignout = async (formValues: SignoutFormValues) => {
+    console.log(formValues);
+    // TODO: 소셜 연동 해제를 위한 재로그인 로직 필요
+    const requestBody: SignoutRequestBody = {
+      authCode: '',
+      authProvider: '',
+      reason: formValues.reason,
+      opinion: formValues.opinion,
+    };
     setIsLoading(true);
     setError(null);
     try {
       await authApi.signout(requestBody); // 회원 탈퇴 요청
       queryClient.clear(); // 쿼리 캐시 초기화
       useAuthStore.getState().reset(); // 전역 유저 상태 초기화
-      document.cookie = 'accessToken=; Max-Age=0; path=/;'; // accessToken 삭제 및 만료 처리
+      deleteAllCookies(); // accessToken 삭제 및 만료 처리
 
       router.replace('/');
     } catch (error) {
@@ -39,6 +50,6 @@ export const useSignout = () => {
   return {
     isLoading,
     error,
-    requestSignout,
+    handleSignout,
   };
 };
