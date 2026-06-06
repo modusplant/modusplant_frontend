494e824e2de6788e76f84a0993a4714d67618018
aggie97 <4ggie1997@gmail.com>
2026-06-06 18:26:41 +0900

    feat(MP-756): 관리자, 디자인 페이지 생성 및 프록시 설정

---
+++ app/admin/layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { decodeJWT } from '@/lib/utils/auth/decodeJWT';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export const dynamic = 'force-dynamic';
+
+export default async function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('accessToken')?.value;
+
+  if (!token) {
+    redirect('/login');
+  }
+
+  const payload = decodeJWT(token);
+
+  if (!payload || payload.role !== 'ADMIN') {
+    redirect('/');
+  }
+
+  return <>{children}</>;
+}
