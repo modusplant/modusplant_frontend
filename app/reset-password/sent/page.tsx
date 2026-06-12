a9c0925e7c4d50cd9e5f52dd2a01d51d957e95b5
k0nghaa <k0nghaalee@gmail.com>
2026-06-13 01:52:30 +0900

    feat(MP-734): 모바일용 이메일 전송 확인 컴포넌트 및 페이지 생성
    - 해당 페이지 공개 경로 목록에 추가

---
+++ app/reset-password/sent/page.tsx
@@ -0,0 +1,5 @@
+import ResetPasswordSentContent from '@/components/auth/resetPassword/resetPasswordSentContent';
+
+export default function EmailSentNoticePage() {
+  return <ResetPasswordSentContent />;
+}
---
+++ components/auth/resetPassword/emailSentNotice.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function EmailSentNotice() {
+  return (
+    <div className="mx-auto flex max-w-80 flex-col gap-10">
+      <div className="flex flex-col items-center gap-6">
+        {/* TODO: 아이콘 변경(배경 투명한 체크 아이콘 필요) */}
+        <Image
+          src="/icon/check.svg"
+          alt="Check Icon"
+          width={120}
+          height={120}
+          loading="lazy"
+        />
+        <p className="text-center">
+          이메일 전송이 완료되었습니다.
+          <br /> 메일함을 확인해주세요.
+        </p>
+      </div>
+      <p className="text-neutral-60 text-center text-sm">
+        메일이 오지 않았다면,{' '}
+        <span className="text-neutral-20">스팸메일함</span>을 확인하거나
+        <br />
+        <Link
+          href="/reset-password"
+          className="text-neutral-20 underline underline-offset-4 hover:text-neutral-50"
+        >
+          이메일 재전송
+        </Link>
+        을 해주세요.
+      </p>
+    </div>
+  );
+}
